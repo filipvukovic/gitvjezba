f45aa130a89e6d3c730d2336f4f5d18827382ccc
filipvukovic <filip.vukovic854@gmail.com>
2016-08-25 11:45:35 +0200

    Add files via upload
    3 Dan zadatak

---
+++ vjezba03.js
@@ -0,0 +1,9 @@
+var x = document.getElementById "ime";
+var y = document.getElementById "godine";
+var z = document.getElementById "boja";
+li.apendChild(naziv);
+li.style.color = z;
+lista.appendChild(li);
+
+
+
